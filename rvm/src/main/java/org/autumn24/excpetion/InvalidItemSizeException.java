bfb0269e77570b429980e22fdd630949307a221e
evnct <elmerivk@proton.me>
2024-11-12 20:05:32 +0200

    Add custom item size exception

---
+++ rvm/src/main/java/org/autumn24/excpetion/InvalidItemSizeException.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of RVMSystem.
+ *
+ * RVMSystem is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * RVMSystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RVMSystem
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This file is part of RVMSystem.
+ *
+ * RVMSystem is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * RVMSystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RVMSystem
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.autumn24.excpetion;
+
+/**
+ * Thrown when generated random size chosen from the standardSizes array is not
+ * from the array or has unpredictable size which will affect the whole program.
+ * @author evnct
+ * @since 1.0.0
+ */
+public class InvalidItemSizeException extends Throwable {
+    public InvalidItemSizeException(String message) {
+        super(message);
+    }
+}
