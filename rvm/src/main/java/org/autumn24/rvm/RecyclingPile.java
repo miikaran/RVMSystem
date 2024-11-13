7b1782f408f25053ad6427745cc9b3dd4f2e2006
miikaran <42628@edu.raseko.fi>
2024-11-13 20:09:10 +0200

    Added RecyclingPile enum to match item material for.

---
+++ rvm/src/main/java/org/autumn24/rvm/RecyclingPile.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of RVMSystem.
+ *
+ * RVMSystem is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * RVMSystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RVMSystem
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.autumn24.rvm;
+
+/**
+ * Different types of piles for recycled items
+ * @author miikaran
+ * @since 1.0.0
+ */
+public enum RecyclingPile {
+    METAL,
+    PLASTIC,
+    GLASS
+}
