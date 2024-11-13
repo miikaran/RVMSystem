f60e7304819f1300835d98c18b089e108d4a72ab
evnct <elmerivk@proton.me>
2024-11-13 23:38:57 +0200

    Fixed Item interface name

--- rvm/src/main/java/org/autumn24/items/ItemCreation.java
+++ rvm/src/main/java/org/autumn24/items/ItemCreation.java
@@ -24,7 +24,7 @@ import java.util.Random;
  * @author evnct
  * @since 1.0.0
  */
-public interface Item {
+public interface ItemCreation {
     default double selectRandomSize(double[] standardSizes) {
         int rnd = new Random().nextInt(standardSizes.length);
         return standardSizes[rnd];
