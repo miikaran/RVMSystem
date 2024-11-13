eb64b1b8f0b4f3367d7cb9e82eac9fb55f015461
miikaran <42628@edu.raseko.fi>
2024-11-13 20:07:24 +0200

    Added custom exceptions for handling invalid materials

---
+++ rvm/src/main/java/org/autumn24/excpetion/InvalidItemMaterialException.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of RVMSystem.
+ *
+ * RVMSystem is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * RVMSystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RVMSystem
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.autumn24.excpetion;
+
+/**
+ * Thrown when recycled item material is not found in RecyclingPile enum.
+ * @author miikaran
+ * @since 1.0.0
+ */
+public class InvalidItemMaterialException extends RuntimeException {
+    public InvalidItemMaterialException(String message) {
+        super(message);
+    }
+    public InvalidItemMaterialException(String message, Throwable cause) { super(message, cause); }
+}
