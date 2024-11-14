d351f50f042e4dc337761864e4fcd79e39e81f73
miikaran <42628@edu.raseko.fi>
2024-11-14 22:36:47 +0200

    modified application manager to handle sleep mode properly (not so gud)

--- rvm/src/main/java/org/autumn24/ApplicationManager.java
+++ rvm/src/main/java/org/autumn24/ApplicationManager.java
@@ -20,7 +20,9 @@ package org.autumn24;
 import org.autumn24.excpetion.InvalidItemSizeException;
 import org.autumn24.items.Item;
 import org.autumn24.items.ItemFactory;
+import org.autumn24.rvm.InactivityTimer;
 import org.autumn24.rvm.ReverseVendingMachine;
+import org.autumn24.rvm.Status;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -32,43 +34,56 @@ import java.util.Scanner;
  */
 public class ApplicationManager {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    public static final Scanner scanner = new Scanner(System.in);
     private final ReverseVendingMachine rvm;
-    private final UserInterface rvmUI;
+    private final InactivityTimer inactivityTimer;
     private ArrayList<Item> items = new ArrayList<>();
 
-    public ApplicationManager(ReverseVendingMachine rvm, UserInterface rvmUI){
+    public ApplicationManager(ReverseVendingMachine rvm){
         this.rvm = rvm;
-        this.rvmUI = rvmUI;
+        this.inactivityTimer = new InactivityTimer(rvm);
         // Generate random bottles at init
         generateBottles(new ItemFactory());
     }
 
     public void run(){
+        if(rvm.getRvmStatus().equals(Status.OPERATIONAL)){
+            System.out.println("\nStarting Machine '" + rvm.getRvmId() + "'...");
+            rvm.setRvmStatus(Status.INUSE);
+        } else {
+            // Tähän ehkä myöhemmin sellane et jos ei oo toiminnassa ni täytyy employeena eli
+            // vähän niiku adminina käydä korjaamassa se vika siitä koneesta.
+            System.out.println("Machine '" + rvm.getRvmId() + "' is not operational :(");
+        }
         while (true){
-            rvmUI.displayMenu();
+            UserInterface.displayMenu();
             handleActions();
+            inactivityTimer.resetTimer();
         }
     }
 
     private void handleActions(){
         try{
             System.out.print("=> ");
-            int userInput = getUserChoice();
+            int userInput = getUserAction();
             switch (userInput){
                 case 1 -> rvm.recycleItem(items.getFirst());
                 case 2 -> System.out.println("Unwrinkling");
                 case 3 -> rvm.printReceipt();
                 case 4 -> System.out.println("Donating");
                 case 5 -> System.exit(0);
-                default -> throw new Exception("Invalid option...");
+                default -> throw new Exception("Invalid user input");
             }
         } catch(Exception e){
+            if(rvm.getRvmStatus().equals(Status.IDLE)){
+                System.out.println("\n\uD83D\uDD0B Powering up machine: " + rvm.getRvmId());
+                return;
+            }
             System.out.println(e.getMessage());
         }
     }
 
-    private int getUserChoice() throws Exception {
+    private int getUserAction() throws Exception {
         if (!scanner.hasNextInt()) {
             scanner.nextLine();  // Clear invalid input
             throw new Exception("Invalid input, expected an integer.");
