dd2baccdcb426042390d1293fbb336c955d6ee0a
miikaran <42628@edu.raseko.fi>
2024-11-19 00:09:41 +0200

    Added auth manager to authenticate users

---
+++ rvm/src/main/java/org/autumn24/AuthManager.java
@@ -0,0 +1,50 @@
+/*
+ * This file is part of RVMSystem.
+ *
+ * RVMSystem is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * RVMSystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RVMSystem
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.autumn24;
+
+import org.autumn24.users.User;
+
+import java.util.ArrayList;
+
+public class AuthManager {
+	private final AppDataManager appDataManager;
+
+	public AuthManager(AppDataManager appDataManager) {
+		this.appDataManager = appDataManager;
+	}
+
+	public boolean authenticateUser(String userId) {
+		ArrayList<User> userData = appDataManager.appData.getUserData();
+		for (User user : userData) {
+			if (user.getUserId().equals(userId)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public User getUserById(String userId) {
+		ArrayList<User> userData = appDataManager.appData.getUserData();
+		for (User user : userData) {
+			if (user.getUserId().equals(userId)) {
+				return user;
+			}
+		}
+		return null;
+	}
+}
