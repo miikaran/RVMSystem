7590a8fefa81033d86a6fdf541b810b452820df0
evnct <elmerivk@proton.me>
2024-11-19 09:33:14 +0200

    Create a custom InvalidOptionException

---
+++ rvm/src/main/java/org/autumn24/exceptions/InvalidOptionException.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of RVMSystem.
+ *
+ * RVMSystem is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * RVMSystem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RVMSystem
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.autumn24.exceptions;
+
+/**
+ * Used when input is expected to be some listed predetermined value.
+ */
+public class InvalidOptionException extends IllegalArgumentException {
+	public InvalidOptionException(String message) {
+		super(message);
+	}
+
+	public InvalidOptionException() {
+		super("invalid option...");
+	}
+}
