fc0494afda0c016ad29cf3945991a8654e35473f
evnct <elmerivk@proton.me>
2024-11-23 12:19:03 +0200

    Included total energy saved method, public => protected

--- rvm/src/main/java/org/autumn24/enviromental_impact/EnergySaved.java
+++ rvm/src/main/java/org/autumn24/enviromental_impact/EnergySaved.java
@@ -19,6 +19,8 @@ package org.autumn24.enviromental_impact;
 
 import org.autumn24.users.RegisteredRecycler;
 
+import java.math.BigDecimal;
+
 /**
  * This class represents how much energy recyclers recycling has saved.
  */
@@ -29,7 +31,7 @@ public class EnergySaved {
 	 * @param recycler the user whose recycling data is used in the calculation
 	 * @return energy saved (Kilowatt Hour)
 	 */
-	public static double energySavedByRecyclingAluminiumCans(RegisteredRecycler recycler) {
+	protected static BigDecimal energySavedByRecyclingAluminiumCans(RegisteredRecycler recycler) {
 		// https://www.madehow.com/Volume-2/Aluminum-Beverage-Can.html
 		// Recycling aluminum can save 95% of energy costs used to produce aluminum
 		final double producingVirginAluminumCan = 1.5; // ~ MJ
@@ -41,7 +43,7 @@ public class EnergySaved {
 	 * @param recycler the user whose recycling data is used in the calculation
 	 * @return energy saved (Kilowatt Hour)
 	 */
-	public static double energySavedByRecyclingPlasticBottles(RegisteredRecycler recycler) {
+	protected static BigDecimal energySavedByRecyclingPlasticBottles(RegisteredRecycler recycler) {
 		// https://www.greenbubble.com.au/blogs/news/how-much-energy-does-it-take-to-recycle-a-plastic-bottle
 		// https://pacinst.org/wp-content/uploads/2013/02/bottled_water_and_energy3.pdf
 		final double producingVirginPlasticBottle = 3.4; // ~ per litre bottle
@@ -53,7 +55,7 @@ public class EnergySaved {
 	 * @param recycler the user whose recycling data is used in the calculation
 	 * @return energy saved (Kilowatt Hour)
 	 */
-	public static double energySavedByRecyclingGlassBottles(RegisteredRecycler recycler) {
+	protected static BigDecimal energySavedByRecyclingGlassBottles(RegisteredRecycler recycler) {
 		// https://learnglassblowing.com/the-energy-used-to-make-a-glass-bottle/
 		// www.norcalcompactors.net/glass-recycling/
 		final double producingVirginGlassBottle = 1.8;
@@ -61,19 +63,30 @@ public class EnergySaved {
 		return calculateEnergySaved(recycler, producingVirginGlassBottle, recyclingAGlassBottle);
 	}
 
-	private static double calculateEnergySaved(
+	/**
+	 * @param recycler recycler the user whose recycling data is used in the calculation
+	 * @return energy saved (Kilowatt Hour)
+	 */
+	protected static BigDecimal energySavedInTotal(RegisteredRecycler recycler) {
+		return energySavedByRecyclingPlasticBottles(recycler)
+				.add(energySavedByRecyclingGlassBottles(recycler)
+						.add(energySavedByRecyclingAluminiumCans(recycler))
+				);
+	}
+
+	private static BigDecimal calculateEnergySaved(
 			RegisteredRecycler recycler,
 			double energyRequiredToProduce, // MJ
 			double energyRequiredToRecycle  // MJ
 	) {
 		long cansRecycled = recycler.getTotalAluminiumCansRecycled();
-		double energySavedByCan = (energyRequiredToProduce - energyRequiredToRecycle);
+		double energySavedByCan = energyRequiredToProduce - energyRequiredToRecycle;
 		double energySaved = energySavedByCan * cansRecycled;
 		return convertMJToKiloWattHour(energySaved);
 	}
 
-	private static double convertMJToKiloWattHour(double mjValueToBeConverted) {
+	private static BigDecimal convertMJToKiloWattHour(double mjValueToBeConverted) {
 		// 1 MJ = 0.27778 kwh
-		return mjValueToBeConverted * 0.27778;
+		return BigDecimal.valueOf(mjValueToBeConverted * 0.27778);
 	}
 }
